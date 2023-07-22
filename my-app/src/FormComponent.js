9dd7f23aba9eef7e3124bfb63f231666a8473940
Macolister <bispo.mac@gmail.com>
2023-07-21 22:16:28 -0400

    write form skeleton

---
+++ my-app/src/FormComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+import {useFormik} from "formik"
+import { object, string, number} from 'yup'
+
+function CarForm({}){
+
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default CarForm;
