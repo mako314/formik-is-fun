94b5e99c40a54471405ceaf4a9a19684657389ac
Macolister <bispo.mac@gmail.com>
2023-07-21 23:21:13 -0400

    import route, do routes and correct fetch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { useNavigate, Route, Routes } from 'react-router-dom';
 import FormComponent from './FormComponent'
 
 function App() {
   const [cars, setCars] = useState([])
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5555/equipment")
+    fetch("http://127.0.0.1:5555/cars")
       .then((resp) => resp.json())
       .then((data) => {
         setCars(data)
@@ -37,7 +38,9 @@ function App() {
 
   return (
     <div className="App">
+      <Routes>
         <Route path='/' element={<FormComponent addCar={addCar}/>} />
+      </Routes>
         {mappedCars}
     </div>
   );
