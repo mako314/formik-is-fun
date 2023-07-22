a37746555adf664dfed16f99b3e0a964c4a5c3c8
Macolister <bispo.mac@gmail.com>
2023-07-21 22:27:00 -0400

    write useEffect to get data with state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [cars, setCars] = useState([])
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5555/equipment")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCars(data)
+      })
+  }, [])
+
+  const addCar = (car) => {
+    setCars(cars => [...cars, car])
+  }
+
+
+
   return (
     <div className="App">
 
