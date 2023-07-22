ed9415bfb79038538cb9d7a5e00d9d1c875ea8b0
Macolister <bispo.mac@gmail.com>
2023-07-21 21:31:34 -0400

    add models.py and car model

---
+++ server/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy()
+
+class Car(db.Model, SerializerMixin):
+    __tablename__ = "cars"
+
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String)
+    model = db.Column(db.Integer)
+    year = db.Column(db.String)
+    price = db.Column(db.String)
