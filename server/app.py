ebf7825d1a43648dc58edb6250cfc89925d76222
Macolister <bispo.mac@gmail.com>
2023-07-21 21:57:52 -0400

    write app.py get and post for Cars

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from models import db, Car
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask import Flask, make_response, jsonify
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -22,6 +22,34 @@ db.init_app(app)
 api = Api(app)
 CORS(app)
 
+class Cars(Resource):
+    # This is our GET. We'll be using this to make sure we're getting our data, and posting succesfully.
+    def get(self):
+        cars = [car.to_dict() for car in Car.query.all()]
+
+        return make_response(cars, 200)
+    
+    # This is our POST. We'll use it to receive the information from the frontend
+    def post(self):
+        # You'll need this line of code to properly capture the data in your post.
+        data = request.get_json()
+
+        # We won't do validations this time around, but if requested I can add it.
+        new_car = Car(
+            make = data['make'],
+            model = data['model'],
+            year = data['data'],
+            price = data['price']
+        )
+
+        #This line actually adds our new_car to our DB.
+        db.session.add(new_car)
+        #The line below, commits the car to the database.
+        db.session.commit()
+
+        return make_response(new_car.to_dict(), 201)
+    
+api.add_resource(Cars, '/cars')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
